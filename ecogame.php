2cf4d7c51fdc826749b1bdca67ea0d16e6ce07c3
jop064 <joaopedrocnsd2017@gmail.com>
2025-06-04 01:33:11 -0300

    Create ecogame.php

---
+++ ecogame.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <a href="formulario-compra.html" class="btn-comprar">Comprar</a>
+    <style>
+        .btn-comprar {
+  display: inline-block;
+  padding: 10px 20px;
+  background-color: #4CAF50;
+  color: white;
+  text-decoration: none;
+  border-radius: 5px;
+  font-weight: bold;
+  transition: background-color 0.3s ease;
+}
+
+.btn-comprar:hover {
+  background-color: #388e3c;
+}
+</style>
+</body>
+</html>
